fa47feaf1a8ad7688adbb2331f4f89a1cafb88ff
Brian Martin <bmartin@twitter.com>
2020-09-30 14:03:17 -0700

    drop floating point math for even more speedup
    Make some changes to avoid floating point conversions and math
    in indexing. Gets another 20-30% on sub_assign() slow path and
    maybe 10-15% on increment.

--- histogram/src/indexing/u8.rs
+++ histogram/src/indexing/u8.rs
@@ -30,7 +30,7 @@ impl crate::Indexing for u8 {
             let power = if value < 100 { 1 } else { 2 };
             let denominator = 10_usize.pow((power - precision + 1).into());
             let power_offset =
-                (0.9_f64 * f64::from(exact as u32 * (power as u32 - precision as u32))) as usize;
+                9 * exact as usize * (power as usize - precision as usize) / 10;
             let remainder: usize = value as usize / denominator;
             let shift = exact as usize / 10;
             let index = exact as usize + power_offset + remainder - shift;
@@ -56,7 +56,7 @@ impl crate::Indexing for u8 {
             let base_offset = 10_usize.pow(precision.into());
             let shift = base_offset / 10;
             let power = precision as usize + (index - base_offset) / (9 * shift);
-            let power_offset = (0.9 * (base_offset * (power - precision as usize)) as f64) as usize;
+            let power_offset = 9 * (shift * (power - precision as usize));
             let value = (index + shift - base_offset - power_offset) as u8
                 * 10_u8.pow((power - precision as usize + 1) as u32);
             Ok(value)
