96866e8c9cfe6e6a36364c6cb0a89d9ce633b734
Brian Martin <bmartin@twitter.com>
2020-10-12 14:01:02 -0700

    split out the valgrind stuff into a separate workflow

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -13,22 +13,5 @@ function test {
 	cargo test --release
 }
 
-
-## Install Valgrind and libc debugging symbols
-sudo apt-get --yes install libc6-dbg
-
-pushd /tmp
-curl -L -O https://sourceware.org/pub/valgrind/valgrind-3.16.1.tar.bz2
-tar xjf valgrind-3.16.1.tar.bz2
-cd valgrind-3.16.1
-./configure
-sudo make -j2 install
-popd
-
-
-
-# Run tests with Valgrind
-test "valgrind --suppressions=build/valgrind-suppressions.supp --error-exitcode=1"
-
 # Run tests without Valgrind
 test
