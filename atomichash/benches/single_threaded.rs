2a8ab86fcaa18b90ce1b234a57abdbfe589b4d5b
Brian Martin <bmartin@twitter.com>
2020-10-12 12:39:30 -0700

    WIP: atomichash: initial commit
    Initial commit of a hashmap implemented with atomic pointers for
    each entry instead of using mutex/rwlock.
    
    This is useful primarily if the values have interior mutability,
    for example if the values are atomics.
    
    Provides speed benefits over standard hashmap for lookups at cost
    of a higher insert cost. Unlike a standard hashmap or other
    concurrent hashmap implementations, the current implementation does
    not resize the hash table.

---
+++ atomichash/benches/single_threaded.rs
@@ -0,0 +1,74 @@
+use rand::distributions::Distribution;
+use rand::thread_rng;
+use rand::distributions::Uniform;
+use rustcommon_atomichash::AtomicHashMap;
+use std::sync::Arc;
+use criterion::Throughput;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn u64_u64(c: &mut Criterion) {
+    let map = Arc::new(AtomicHashMap::<u64, u64>::with_capacity(1000000));
+
+    let mut group = c.benchmark_group("atomichash/u64/u64");
+
+    group.throughput(Throughput::Elements(1));
+
+    let mut rng = thread_rng();
+    let distribution = Uniform::new_inclusive(0.0, u32::MAX as f64);
+    
+    let mut value = distribution.sample(&mut rng).floor() as u64;
+    // pre-fill to 50% occupancy
+    for _ in 0..500000 {
+        let _ = map.insert(value, value);
+        value = distribution.sample(&mut rng).floor() as u64;
+    }
+
+    group.bench_function("insert/50%", |b| {
+        b.iter(|| {
+            let _ = map.insert(value, value);
+        })
+    });
+
+    group.bench_function("get/50%", |b| {
+        b.iter(|| {
+            map.get(&value);
+        })
+    });
+}
+
+fn std_hashmap(c: &mut Criterion) {
+    let mut map = std::collections::HashMap::new();
+
+    let mut group = c.benchmark_group("std/u64/u64");
+
+    group.throughput(Throughput::Elements(1));
+
+    let mut rng = thread_rng();
+    let distribution = Uniform::new_inclusive(0.0, u32::MAX as f64);
+    
+    let mut value = distribution.sample(&mut rng).floor() as u64;
+    // pre-fill to 50% occupancy
+    for _ in 0..500000 {
+        map.insert(value, value);
+        value = distribution.sample(&mut rng).floor() as u64;
+    }
+
+    group.bench_function("insert/50%", |b| {
+        b.iter(|| {
+            map.insert(value, value);
+        })
+    });
+
+    group.bench_function("get/50%", |b| {
+        b.iter(|| {
+            map.get(&value);
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    u64_u64,
+    std_hashmap,
+);
+criterion_main!(benches);
