38ae1c58707999b8e46b09450a64fb6c018b9ac0
Brian Martin <bmartin@twitter.com>
2020-10-13 15:08:20 -0700

    add benchmarking comparison to CI

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -3,7 +3,16 @@
 set -e
 
 export RUST_BACKTRACE=1
+export BRANCH=$(git branch | grep \* | awk '{print $2}')
 
 cargo check
 cargo build --release
 cargo test --release
+
+echo "Getting previous benchmark results"
+git checkout master
+cargo bench
+
+echo "Calculating performance change"
+git checkour $BRANCH
+cargo bench
