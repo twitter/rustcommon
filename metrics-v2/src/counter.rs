8a49240835b4ffa896dda77941c6199d92638de2
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-01 15:56:14 -0400

    Port in hamerkop metrics implementation
    This brings in the subset of the metrics implementation from
    https://github.com/phantomical/hamerkop for which the design is straightforward.

---
+++ metrics-v2/src/counter.rs
@@ -0,0 +1,67 @@
+use crate::Metric;
+use std::{
+    any::Any,
+    sync::atomic::{AtomicU64, Ordering},
+};
+
+/// A counter. Can be incremented or added to.
+///
+/// # Example
+/// ```
+/// # use metrics_v2::{metric, Counter};
+/// metric! {
+///     #[name = "my.custom.metric"]
+///     static MY_COUNTER: Counter = Counter::new();
+/// }
+///
+/// fn a_method() {
+///     MY_COUNTER.increment();
+///     // ...
+/// }
+/// # a_method();
+/// ```
+#[derive(Default, Debug)]
+pub struct Counter(AtomicU64);
+
+impl Counter {
+    /// Create a counter initialized to 0.
+    pub const fn new() -> Self {
+        Self::with_value(0)
+    }
+
+    /// Create a counter initialized to `value`.
+    pub const fn with_value(value: u64) -> Self {
+        Self(AtomicU64::new(value))
+    }
+
+    #[inline]
+    pub fn increment(&self) -> u64 {
+        self.add(1)
+    }
+
+    #[inline]
+    pub fn add(&self, value: u64) -> u64 {
+        self.0.fetch_add(value, Ordering::Relaxed)
+    }
+
+    #[inline]
+    pub fn value(&self) -> u64 {
+        self.0.load(Ordering::Relaxed)
+    }
+
+    #[inline]
+    pub fn set(&self, value: u64) -> u64 {
+        self.0.swap(value, Ordering::Relaxed)
+    }
+
+    #[inline]
+    pub fn reset(&self) -> u64 {
+        self.set(0)
+    }
+}
+
+impl Metric for Counter {
+    fn as_any(&self) -> Option<&dyn Any> {
+        Some(self)
+    }
+}
---
+++ metrics-v2/src/lib.rs
@@ -0,0 +1,128 @@
+//! Easily registered distributed metrics.
+//!
+//! More docs todo...
+//!
+//! # Creating a Metric
+//! Registering a metric is straightforward. All that's needed is to declare a
+//! static within the [`metric!`] macro. By default, the metric will have the
+//! name of the path to the static variable you used to declare it but this can
+//! be overridden by adding a `#[name]` attribute.
+//!
+//! ```
+//! # use metrics_v2::{metric, Counter};
+//! metric! {
+//!     /// A counter metric named "<crate name>::COUNTER_A"
+//!     static COUNTER_A: Counter = Counter::new();
+//!
+//!     /// A counter metric named "my.metric.name"
+//!     #[name = "my.metric.name"]
+//!     static COUNTER_B: Counter = Counter::new();
+//! }
+//! ```
+//!
+//! # Accessing Metrics
+//! All metrics registered via the [`metric!`] macro can be accessed by calling
+//! the [`metrics`] function. This will return a slice with one [`MetricEntry`]
+//! instance per metric that was registered via the [`metric!`] macro.
+//!
+//! Suppose we have the metrics declared in the example above.
+//! ```
+//! # // This should remain in sync with the example above.
+//! # use metrics_v2::{metric, Counter};
+//! # metric! {
+//! #     /// A counter metric named "COUNTER_A"
+//! #     static COUNTER_A: Counter = Counter::new();
+//! #
+//! #     /// A counter metric named "my.metric.name"
+//! #     #[name = "my.metric.name"]
+//! #     static COUNTER_B: Counter = Counter::new();
+//! # }
+//! # use metrics_v2::metrics;
+//! let metrics = metrics();
+//! // Metrics may be in any arbitrary order
+//! let mut names: Vec<_> = metrics.iter().map(|metric| metric.name()).collect();
+//! names.sort();
+//!
+//! assert_eq!(names[0], "my.metric.name");
+//! assert_eq!(names[1], concat!(module_path!(), "::", "COUNTER_A"));
+//! ```
+
+use std::any::Any;
+use std::fmt;
+
+mod counter;
+mod gauge;
+mod macros;
+
+pub use crate::counter::Counter;
+pub use crate::gauge::Gauge;
+
+#[doc(hidden)]
+pub mod export {
+    pub extern crate linkme;
+
+    #[linkme::distributed_slice]
+    pub static METRICS: [crate::MetricEntry] = [..];
+}
+
+/// The list of all metrics registered via the [`metric!`] macro.
+///
+/// Names within metrics are not guaranteed to be unique and no aggregation of
+/// metrics with the same name is done.
+pub fn metrics() -> &'static [MetricEntry] {
+    &*crate::export::METRICS
+}
+
+/// Global interface to a metric.
+///
+/// Most use of metrics should use the directly declared constants.
+pub trait Metric: Sync {
+    /// Indicate whether this metric has been set up.
+    ///
+    /// Generally, if this returns `false` then the other methods on this
+    /// trait should return `None`.
+    fn is_enabled(&self) -> bool {
+        true
+    }
+
+    /// Get the current metric as an [`Any`] instance. This is meant to allow
+    /// custom processing for known metric types.
+    ///
+    /// [`Any`]: std::any::Any
+    fn as_any(&self) -> Option<&dyn Any>;
+}
+
+/// A statically declared metric entry.
+pub struct MetricEntry {
+    #[doc(hidden)]
+    pub metric: &'static dyn Metric,
+    #[doc(hidden)]
+    pub name: &'static str,
+}
+
+impl MetricEntry {
+    pub fn metric(&self) -> &'static dyn Metric {
+        self.metric
+    }
+
+    pub fn name(&self) -> &'static str {
+        self.name
+    }
+}
+
+impl std::ops::Deref for MetricEntry {
+    type Target = dyn Metric;
+
+    fn deref(&self) -> &Self::Target {
+        self.metric()
+    }
+}
+
+impl fmt::Debug for MetricEntry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MetricEntry")
+            .field("name", &self.name())
+            .field("metric", &"<dyn Metric>")
+            .finish()
+    }
+}
