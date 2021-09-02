822b186f9cc01e0560c1718c5fe9b858dcaa405a
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-02 14:38:11 -0400

    Implement metric attribute macro

---
+++ metrics-v2/derive/src/lib.rs
@@ -0,0 +1,11 @@
+use proc_macro::TokenStream;
+
+mod metric;
+
+#[proc_macro_attribute]
+pub fn metric(attr: TokenStream, item: TokenStream) -> TokenStream {
+    match metric::metric(attr, item) {
+        Ok(tokens) => tokens.into(),
+        Err(e) => e.to_compile_error().into()
+    }
+}
