twitter/rustcommon

commit b474bbc5bffff756a0a42f711d60faf918584966

replace histogram with base-2 implementation (#122)
Replaces the histogram with a simple base-2 implementation which is
opinionated about the values and counter widths. This simplifies
usage by converging on a single implementation using atomic
counters. Additionally, we get substantial performance benefits by
switching to a base-2 implementation.

metrics/src/lib.rs
@@ -72,10 +72,8 @@ use std::borrow::Cow;
 
 mod counter;
 mod gauge;
-mod lazy;
-
-#[cfg(feature = "heatmap")]
 mod heatmap;
+mod lazy;
 
 extern crate self as rustcommon_metrics;
 
@@ -84,13 +82,10 @@ pub mod dynmetrics;
 pub use crate::counter::Counter;
 pub use crate::dynmetrics::{DynBoxedMetric, DynPinnedMetric};
 pub use crate::gauge::Gauge;
+pub use crate::heatmap::Heatmap;
 pub use crate::lazy::{Lazy, Relaxed};
 
-#[cfg(feature = "heatmap")]
-pub use crate::heatmap::Heatmap;
-
 pub use rustcommon_metrics_derive::{metric, to_lowercase};
-
 pub use rustcommon_time::*;
 
 #[doc(hidden)]

heatmap/benches/heatmap.rs
@@ -1,0 +1,22 @@
+use criterion::Throughput;
+use criterion::{criterion_group, criterion_main, Criterion};
+use heatmap::*;
+
+fn heatmap(c: &mut Criterion) {
+    let heatmap = Heatmap::new(0, 4, 20, Duration::from_secs(1), Duration::from_millis(1)).unwrap();
+
+    let mut group = c.benchmark_group("heatmap");
+
+    group.throughput(Throughput::Elements(1));
+    let mut time = Instant::now();
+    let interval = Duration::from_millis(1);
+    group.bench_function("increment", |b| {
+        b.iter(|| {
+            time += interval;
+            heatmap.increment(Instant::now(), 1, 1)
+        })
+    });
+}
+
+criterion_group!(benches, heatmap);
+criterion_main!(benches);

heatmap/src/heatmap.rs
@@ -1,0 +1,326 @@
+// Copyright 2020 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+use crate::Error;
+use crate::*;
+use core::sync::atomic::*;
+
+use histogram::{Bucket, Histogram};
+
+/// A `Heatmap` stores counts for timestamped values over a configured span of
+/// time.
+///
+/// Internally, it is represented as a ring buffer of histograms with one
+/// additional histogram to track all counts within the span of time. Old
+/// histograms age-out as time moves forward and they are subtracted from the
+/// summary histogram at that point.
+///
+/// This acts as a moving histogram, such that requesting a percentile returns
+/// a percentile from across the configured span of time.
+pub struct Heatmap {
+    slices: Vec<Histogram>,
+    current: AtomicUsize,
+    next_tick: AtomicInstant,
+    resolution: Duration,
+    summary: Histogram,
+}
+
+/// A `Builder` allows for constructing a `Heatmap` with the desired
+/// configuration.
+pub struct Builder {
+    // minimum resolution parameter `M = 2^m`
+    m: u32,
+    // minimum resolution range parameter `R = 2^r - 1`
+    r: u32,
+    // maximum value parameter `N = 2^n - 1`
+    n: u32,
+    // span of time represented by the heatmap
+    span: Duration,
+    // the resolution in the time domain
+    resolution: Duration,
+}
+
+impl Builder {
+    /// Consume the `Builder` and return a `Heatmap`.
+    pub fn build(self) -> Result<Heatmap, Error> {
+        Heatmap::new(self.m, self.r, self.n, self.span, self.resolution)
+    }
+
+    /// Sets the width of the smallest bucket in the `Heatmap`.
+    ///
+    /// As the `Heatmap` uses base-2 internally, the resolution will be the
+    /// largest power of two that is less than or equal to the provided value.
+    /// For example, if the minimum resolution is set to 10, the width of the
+    /// smallest bucket will be 8.
+    pub fn min_resolution(mut self, width: u64) -> Self {
+        self.m = 64 - width.leading_zeros();
+        self
+    }
+
+    /// Sets the maximum value that the minimum resolution extends to.
+    ///
+    /// This value should be greater than the minimum resolution. If the value
+    /// provided is not a power of two, the smallest power of two that is larger
+    /// than the provided value will be used.
+    pub fn min_resolution_range(mut self, value: u64) -> Self {
+        self.r = 64 - value.next_power_of_two().leading_zeros();
+        self
+    }
+
+    /// Sets the maximum value that can be recorded into the `Heatmap`.
+    ///
+    /// If the value provided is not a power of two, the smallest power of two
+    /// that is larger than the provided value will be used.
+    pub fn maximum_value(mut self, value: u64) -> Self {
+        self.n = 64 - value.next_power_of_two().leading_zeros();
+        self
+    }
+
+    /// Sets the duration that is covered by the `Heatmap`.
+    ///
+    /// Values that are older than the duration will be dropped as they age-out.
+    pub fn span(mut self, duration: Duration) -> Self {
+        self.span = duration;
+        self
+    }
+
+    /// Sets the resolution in the time domain.
+    ///
+    /// Increments with similar timestamps will be grouped together and age-out
+    /// together.
+    pub fn resolution(mut self, duration: Duration) -> Self {
+        self.resolution = duration;
+        self
+    }
+}
+
+impl Heatmap {
+    /// Create a new `Heatmap` which stores counts for timestamped values over
+    /// a configured span of time.
+    ///
+    /// - `m` - sets the minimum resolution `M = 2^m`. This is the smallest unit
+    /// of quantification, which is also the smallest bucket width. If the input
+    /// values are always integers, choosing `m=0` would ensure precise
+    /// recording for the smallest values.
+    ///
+    /// - `r` - sets the minimum resolution range `R = 2^r - 1`. The selected
+    /// value must be greater than the minimum resolution `m`. This sets the
+    /// maximum value that the minimum resolution should extend to.
+    ///
+    /// - `n` - sets the maximum value `N = 2^n - 1`. The selected value must be
+    /// greater than or equal to the minimum resolution range `r`.
+    ///
+    /// - `span` - sets the total duration that the heatmap covers
+    ///
+    /// - `resolution` - sets the resolution in the time domain. Counts from
+    /// similar instants in time will be grouped together.
+    pub fn new(
+        m: u32,
+        r: u32,
+        n: u32,
+        span: Duration,
+        resolution: Duration,
+    ) -> Result<Self, Error> {
+        let mut slices = Vec::new();
+        let mut true_span = Duration::from_nanos(0);
+        while true_span < span {
+            slices.push(Histogram::new(m, r, n)?);
+            true_span += resolution;
+        }
+        slices.shrink_to_fit();
+        let next_tick = AtomicInstant::now();
+        next_tick.fetch_add(resolution, Ordering::Relaxed);
+        Ok(Self {
+            slices,
+            current: AtomicUsize::new(0),
+            next_tick,
+            resolution,
+            summary: Histogram::new(m, r, n)?,
+        })
+    }
+
+    /// Creates a `Builder` with the default values `m = 0`, `r = 10`, `n = 30`,
+    /// `span = 60s`, `resolution = 1s`.
+    ///
+    /// This would create a `Heatmap` with 61 total `Histogram`s, each with
+    /// 11264 buckets which can store values from 1 to 1_073_741_823 with
+    /// values 1 to 1023 being stored in buckets with a width of 1. Such a
+    /// `Heatmap` would be appropriate for latencies measured in nanoseconds
+    /// where the max expected latency is one second and reporting covers the
+    /// past minute.
+    pub fn builder() -> Builder {
+        Builder {
+            m: 0,
+            r: 10,
+            n: 30,
+            span: Duration::from_secs(60),
+            resolution: Duration::from_secs(1),
+        }
+    }
+
+    /// Returns the number of windows stored in the `Heatmap`
+    pub fn windows(&self) -> usize {
+        self.slices.len()
+    }
+
+    /// Returns the number of buckets stored within each `Histogram` in the
+    /// `Heatmap`
+    pub fn buckets(&self) -> usize {
+        self.summary.buckets()
+    }
+
+    /// Increment a time-value pair by a specified count
+    pub fn increment(&self, time: Instant, value: u64, count: u32) {
+        self.tick(time);
+        if let Some(slice) = self.slices.get(self.current.load(Ordering::Relaxed)) {
+            let _ = slice.increment(value, count);
+            let _ = self.summary.increment(value, count);
+        }
+    }
+
+    /// Return the nearest value for the requested percentile (0.0 - 100.0)
+    /// across the total range of samples retained in the `Heatmap`.
+    ///
+    /// Note: since the heatmap stores a distribution across a configured time
+    /// span, sequential calls to fetch the percentile might result in different
+    /// results even without concurrent writers. For instance, you may see a
+    /// 90th percentile that is higher than the 100th percentile depending on
+    /// the timing of calls to this function and the distribution of your data.
+    ///
+    /// Note: concurrent writes may also effect the value returned by this
+    /// function. Users needing better consistency should ensure that other
+    /// threads are not writing into the heatmap while this function is
+    /// in-progress.
+    pub fn percentile(&self, percentile: f64) -> Result<Bucket, Error> {
+        self.tick(Instant::now());
+        self.summary.percentile(percentile).map_err(Error::from)
+    }
+
+    // Internal function which handles reuse of older windows to store newer
+    /// values.
+    fn tick(&self, time: Instant) {
+        loop {
+            let next_tick = self.next_tick.load(Ordering::Relaxed);
+            if time < next_tick {
+                return;
+            } else {
+                self.next_tick.fetch_add(self.resolution, Ordering::Relaxed);
+                self.current.fetch_add(1, Ordering::Relaxed);
+                if self.current.load(Ordering::Relaxed) >= self.slices.len() {
+                    self.current.store(0, Ordering::Relaxed);
+                }
+                let current = self.current.load(Ordering::Relaxed);
+                if let Some(slice) = self.slices.get(current) {
+                    let _ = self.summary.subtract(slice);
+                    slice.clear();
+                }
+            }
+        }
+    }
+
+    /// Internal function to return a `Window` from the `Heatmap`.
+    fn get_slice(&self, index: usize) -> Option<Window> {
+        if let Some(histogram) = self.slices.get(index) {
+            let shift = if index > self.current.load(Ordering::Relaxed) {
+                self.resolution.mul_f64(
+                    (self.slices.len() + self.current.load(Ordering::Relaxed) - index) as f64,
+                )
+            } else {
+                self.resolution
+                    .mul_f64((self.current.load(Ordering::Relaxed) - index) as f64)
+            };
+            Some(Window {
+                start: self.next_tick.load(Ordering::Relaxed) - shift - self.resolution,
+                stop: self.next_tick.load(Ordering::Relaxed) - shift,
+                histogram,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl Clone for Heatmap {
+    fn clone(&self) -> Self {
+        let slices = self.slices.clone();
+        let summary = self.summary.clone();
+        let resolution = self.resolution;
+        let current = AtomicUsize::new(self.current.load(Ordering::Relaxed));
+        let next_tick = AtomicInstant::new(self.next_tick.load(Ordering::Relaxed));
+
+        Heatmap {
+            slices,
+            current,
+            next_tick,
+            resolution,
+            summary,
+        }
+    }
+}
+
+pub struct Iter<'a> {
+    inner: &'a Heatmap,
+    index: usize,
+    visited: usize,
+}
+
+impl<'a> Iter<'a> {
+    fn new(inner: &'a Heatmap) -> Iter<'a> {
+        let index = if inner.current.load(Ordering::Relaxed) < (inner.slices.len() - 1) {
+            inner.current.load(Ordering::Relaxed) + 1
+        } else {
+            0
+        };
+        Iter {
+            inner,
+            index,
+            visited: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = Window<'a>;
+
+    fn next(&mut self) -> Option<Window<'a>> {
+        if self.visited >= self.inner.slices.len() {
+            None
+        } else {
+            let bucket = self.inner.get_slice(self.index);
+            self.index += 1;
+            if self.index >= self.inner.slices.len() {
+                self.index = 0;
+            }
+            self.visited += 1;
+            bucket
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a Heatmap {
+    type Item = Window<'a>;
+    type IntoIter = Iter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter::new(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn age_out() {
+        let heatmap =
+            Heatmap::new(0, 4, 20, Duration::from_secs(1), Duration::from_millis(1)).unwrap();
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Err(Error::Empty));
+        heatmap.increment(Instant::now(), 1, 1);
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Ok(1));
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Ok(1));
+        std::thread::sleep(std::time::Duration::from_millis(2000));
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Err(Error::Empty));
+    }
+}

heatmap/src/lib.rs
@@ -3,16 +3,20 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 
 mod error;
-mod heatmaps;
+mod heatmap;
 mod window;
 
-pub use error::HeatmapError;
-pub use heatmaps::{AtomicHeatmap, Heatmap};
+use core::sync::atomic::AtomicU64;
+use rustcommon_time::Nanoseconds;
+
+pub use self::heatmap::Heatmap;
+pub use error::Error;
 pub use window::Window;
 
-pub use rustcommon_atomics::{Atomic, AtomicU16, AtomicU32, AtomicU64, AtomicU8};
-pub use rustcommon_histogram::{AtomicCounter, Counter, Indexing};
-pub use rustcommon_time::*;
+pub type Instant = rustcommon_time::Instant<Nanoseconds<u64>>;
+pub type Duration = rustcommon_time::Duration<Nanoseconds<u64>>;
+
+type AtomicInstant = rustcommon_time::Instant<Nanoseconds<AtomicU64>>;
 
 #[cfg(test)]
 mod tests {
@@ -20,32 +24,14 @@ mod tests {
 
     #[test]
     fn age_out() {
-        let mut heatmap = Heatmap::<u64, u64>::new(
-            1_000_000,
-            2,
-            Duration::<Nanoseconds<u64>>::from_secs(1),
-            Duration::<Nanoseconds<u64>>::from_millis(1),
-        );
-        assert_eq!(heatmap.percentile(0.0), Err(HeatmapError::Empty));
-        heatmap.increment(Instant::<Nanoseconds<u64>>::now(), 1, 1);
-        assert_eq!(heatmap.percentile(0.0), Ok(1));
+        let heatmap =
+            Heatmap::new(0, 4, 20, Duration::from_secs(1), Duration::from_millis(1)).unwrap();
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Err(Error::Empty));
+        heatmap.increment(Instant::now(), 1, 1);
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Ok(1));
         std::thread::sleep(std::time::Duration::from_millis(100));
-        assert_eq!(heatmap.percentile(0.0), Ok(1));
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Ok(1));
         std::thread::sleep(std::time::Duration::from_millis(2000));
-        assert_eq!(heatmap.percentile(0.0), Err(HeatmapError::Empty));
-
-        let heatmap = AtomicHeatmap::<u64, AtomicU64>::new(
-            1_000_000,
-            2,
-            Duration::<Nanoseconds<u64>>::from_secs(1),
-            Duration::<Nanoseconds<u64>>::from_millis(1),
-        );
-        assert_eq!(heatmap.percentile(0.0), Err(HeatmapError::Empty));
-        heatmap.increment(Instant::<Nanoseconds<u64>>::now(), 1, 1);
-        assert_eq!(heatmap.percentile(0.0), Ok(1));
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        assert_eq!(heatmap.percentile(0.0), Ok(1));
-        std::thread::sleep(std::time::Duration::from_millis(2000));
-        assert_eq!(heatmap.percentile(0.0), Err(HeatmapError::Empty));
+        assert_eq!(heatmap.percentile(0.0).map(|v| v.high()), Err(Error::Empty));
     }
 }

histogram/src/lib.rs
@@ -1,37 +1,48 @@
-// Copyright 2020 Twitter, Inc.
+// Copyright 2022 Twitter, Inc.
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
 mod bucket;
-mod counter;
 mod error;
-mod histograms;
-mod indexing;
+mod histogram;
+mod percentile;
 
-pub use bucket::*;
-pub use counter::*;
-pub use error::*;
-pub use histograms::*;
-pub use indexing::*;
-
-pub use rustcommon_atomics::{Atomic, AtomicU16, AtomicU32, AtomicU64, AtomicU8};
+pub use self::histogram::{Builder, Histogram};
+pub use bucket::Bucket;
+pub use error::Error;
+pub use percentile::Percentile;
 
 #[cfg(test)]
 mod tests {
-    use crate::*;
+    use super::*;
 
     #[test]
-    fn build() {
-        let h = Histogram::<u8, u8>::new(255, 3);
-        assert_eq!(h.percentile(0.0), Err(HistogramError::Empty));
+    // run some test cases for various histogram sizes
+    fn num_buckets() {
+        let histogram = Histogram::new(0, 2, 10).unwrap();
+        assert_eq!(histogram.buckets(), 20);
 
-        let mut h = Histogram::<u16, u8>::new(10000, 3);
-        assert_eq!(h.percentile(0.0), Err(HistogramError::Empty));
-        h.increment(1, 1);
-        assert_eq!(h.percentile(0.0), Ok(1));
-        assert_eq!(h.percentile(100.0), Ok(1));
-        h.increment(65535, 1);
-        assert_eq!(h.percentile(0.0), Ok(1));
-        assert_eq!(h.percentile(100.0), Err(HistogramError::OutOfRange));
+        let histogram = Histogram::new(0, 10, 20).unwrap();
+        assert_eq!(histogram.buckets(), 6144);
+
+        let histogram = Histogram::new(0, 10, 30).unwrap();
+        assert_eq!(histogram.buckets(), 11264);
+
+        let histogram = Histogram::new(1, 10, 20).unwrap();
+        assert_eq!(histogram.buckets(), 3072);
+
+        let histogram = Histogram::new(0, 9, 20).unwrap();
+        assert_eq!(histogram.buckets(), 3328);
+    }
+
+    #[test]
+    fn percentiles() {
+        let histogram = Histogram::new(0, 2, 10).unwrap();
+
+        for v in 1..1024 {
+            assert!(histogram.increment(v, 1).is_ok());
+            assert!(histogram.percentile(100.0).map(|b| b.high()).unwrap_or(0) >= v);
+            assert!(histogram.percentile(100.0).map(|b| b.low()).unwrap_or(0) <= v);
+        }
     }
 }

metrics/src/heatmap.rs
@@ -3,16 +3,10 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 
 use crate::Metric;
-use rustcommon_atomics::AtomicU64;
-use rustcommon_heatmap::AtomicHeatmap;
 
-pub type Heatmap = AtomicHeatmap<u64, AtomicU64>;
+pub use heatmap::Heatmap;
 
-impl<V, C> Metric for AtomicHeatmap<V, C>
-where
-    V: Send + Sync + 'static,
-    C: Send + Sync + 'static,
-{
+impl Metric for Heatmap {
     fn as_any(&self) -> Option<&dyn std::any::Any> {
         Some(self)
     }

waterfall/src/lib.rs
@@ -7,17 +7,15 @@
 mod palettes;
 
 pub use palettes::Palette;
-use rustcommon_time::{DateTime, Duration};
+use rustcommon_time::DateTime;
 
+use heatmap::*;
 use image::*;
 use palettes::*;
-use rustcommon_heatmap::*;
 use rusttype::{point, Font, PositionedGlyph, Scale as TypeScale};
 
-use core::hash::Hash;
-use core::ops::Sub;
 use std::collections::HashMap;
-use std::convert::{From, TryInto};
+use std::convert::TryInto;
 
 #[derive(Copy, Clone)]
 /// Used to configure various strategies for mapping values to colors
@@ -28,33 +26,29 @@ pub enum Scale {
     Logarithmic,
 }
 
-pub struct WaterfallBuilder<Value> {
+pub struct WaterfallBuilder {
     output: String,
-    labels: HashMap<Value, String>,
+    labels: HashMap<u64, String>,
     palette: Palette,
-    interval: Duration<Nanoseconds<u64>>,
+    interval: Duration,
     scale: Scale,
     smooth: Option<f32>,
 }
 
-impl<Value> WaterfallBuilder<Value>
-where
-    Value: Eq + Hash,
-    u64: From<Value>,
-{
+impl WaterfallBuilder {
     pub fn new(target: &str) -> Self {
         Self {
             output: target.to_string(),
             labels: HashMap::new(),
             palette: Palette::Classic,
-            interval: Duration::<Nanoseconds<u64>>::from_secs(60),
+            interval: Duration::from_secs(60),
             scale: Scale::Linear,
             smooth: None,
         }
     }
 
     /// Adds a label to the horizontal axis at the specified value
-    pub fn label(mut self, value: Value, label: &str) -> Self {
+    pub fn label(mut self, value: u64, label: &str) -> Self {
         self.labels.insert(value, label.to_string());
         self
     }
@@ -86,17 +80,11 @@ where
     }
 
     // find the bucket with the highest weight
-    fn max_weight<Count>(&self, heatmap: &rustcommon_heatmap::Heatmap<Value, Count>) -> f64
-    where
-        Count: rustcommon_heatmap::Counter + PartialOrd + Indexing,
-        Value: rustcommon_heatmap::Indexing + Sub<Output = Value> + Default + PartialOrd,
-        u64: From<Count>,
-        Count: From<u8>,
-    {
+    fn max_weight(&self, heatmap: &heatmap::Heatmap) -> f64 {
         let mut max_weight = 0.0;
         for slice in heatmap {
             for b in slice.histogram() {
-                let weight = self.weight(u64::from(b.count()), u64::from(b.width()));
+                let weight = self.weight(b.count().into(), b.high() - b.low() + 1);
                 if weight > max_weight {
                     max_weight = weight;
                 }
@@ -106,15 +94,9 @@ where
     }
 
     /// Generate the waterfall from the provided heatmap
-    pub fn build<Count>(self, heatmap: &rustcommon_heatmap::Heatmap<Value, Count>)
-    where
-        Count: rustcommon_heatmap::Counter + PartialOrd + Indexing,
-        Value: rustcommon_heatmap::Indexing + Sub<Output = Value> + Default + PartialOrd,
-        u64: From<Count>,
-        Count: From<u8>,
-    {
+    pub fn build(self, heatmap: &heatmap::Heatmap) {
         let now_datetime = DateTime::now();
-        let now_instant = Instant::<Nanoseconds<u64>>::now();
+        let now_instant = Instant::now();
 
         let height = heatmap.windows();
         let width = heatmap.buckets();
@@ -133,7 +115,7 @@ where
 
         let mut labels = HashMap::new();
         for (k, v) in &self.labels {
-            labels.insert(u64::from(*k), v);
+            labels.insert(*k, v);
         }
 
         let mut label_keys: Vec<u64> = labels.keys().cloned().collect();
@@ -147,7 +129,7 @@ where
             // build grayscale buffer
             for (y, slice) in heatmap.into_iter().enumerate() {
                 for (x, b) in slice.histogram().into_iter().enumerate() {
-                    let weight = self.weight(u64::from(b.count()), u64::from(b.width()));
+                    let weight = self.weight(b.count().into(), b.high() - b.low() + 1);
                     let scaled_weight = weight / max_weight;
                     let index = (scaled_weight * (colors.len() - 1) as f64).round() as u8;
                     buf.put_pixel(
@@ -173,7 +155,7 @@ where
             // set the pixels in the buffer
             for (y, slice) in heatmap.into_iter().enumerate() {
                 for (x, b) in slice.histogram().into_iter().enumerate() {
-                    let weight = self.weight(u64::from(b.count()), u64::from(b.width()));
+                    let weight = self.weight(b.count().into(), b.high() - b.low() + 1);
                     let scaled_weight = weight / max_weight;
                     let index = (scaled_weight * (colors.len() - 1) as f64).round() as usize;
                     let color = colors[index];
@@ -190,7 +172,7 @@ where
         if !label_keys.is_empty() {
             let slice = heatmap.into_iter().next().unwrap();
             for (x, bucket) in slice.histogram().into_iter().enumerate() {
-                let value = u64::from(bucket.value());
+                let value = bucket.high();
                 if value >= label_keys[l] {
                     if let Some(label) = labels.get(&label_keys[l]) {
                         render_text(label, 25.0, x, 0, &mut buf);

heatmap/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
-name = "rustcommon-heatmap"
-version = "0.1.3"
+name = "heatmap"
+version = "0.0.0"
 authors = ["Brian Martin <bmartin@twitter.com>"]
 edition = "2021"
 license = "Apache-2.0"
@@ -9,8 +9,7 @@ homepage = "https://github.com/twitter/rustcommon/heatmap"
 repository = "https://github.com/twitter/rustcommon"
 
 [dependencies]
-rustcommon-atomics = { path = "../atomics" }
-rustcommon-histogram = { path = "../histogram" }
+histogram = { path = "../histogram" }
 rustcommon-time = { path = "../time" }
 thiserror = "1.0.34"
 
@@ -18,5 +17,5 @@ thiserror = "1.0.34"
 criterion = "0.3.6"
 
 [[bench]]
-name = "heatmaps"
+name = "heatmap"
 harness = false

commit 562e6373046c1954c0c6161f3c13086944e30a4f

metrics: fix heatmap macro
Heatmap macro was broken by #122

Fixes the heatmap initialization and adds a test to prevent a miss
here in the future.

metrics/src/lib.rs
@@ -140,12 +140,14 @@ macro_rules! heatmap {
             name = rustcommon_metrics::to_lowercase!($name)
         )]
         pub static $name: Relaxed<Heatmap> = Relaxed::new(|| {
-            Heatmap::new(
-                $max as _,
-                3,
-                rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(60),
-                rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(1),
-            )
+            Heatmap::builder()
+                .maximum_value($max as _)
+                .min_resolution(1)
+                .min_resolution_range(1024)
+                .span(rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(60))
+                .resolution(rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(1))
+                .build()
+                .expect("bad heatmap configuration")
         });
     };
     ($name:ident, $max:expr, $description:tt) => {
@@ -154,12 +156,14 @@ macro_rules! heatmap {
             description = $description
         )]
         pub static $name: Relaxed<Heatmap> = Relaxed::new(|| {
-            Heatmap::new(
-                $max as _,
-                3,
-                rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(60),
-                rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(1),
-            )
+            Heatmap::builder()
+                .maximum_value($max as _)
+                .min_resolution(1)
+                .min_resolution_range(1024)
+                .span(rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(60))
+                .resolution(rustcommon_metrics::Duration::<rustcommon_metrics::Nanoseconds<u64>>::from_secs(1))
+                .build()
+                .expect("bad heatmap configuration")
         });
     };
 }

metrics/tests/heatmap.rs
@@ -1,0 +1,27 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+use rustcommon_metrics::*;
+
+heatmap!(LATENCY, 1_000_000_000);
+heatmap!(CARDINALITY, 1_000, "some description");
+
+#[test]
+fn metric_name_as_expected() {
+    let metrics = metrics().static_metrics();
+    assert_eq!(metrics.len(), 2);
+    assert_eq!(metrics[1].name(), "latency");
+    assert_eq!(metrics[1].description(), None);
+}
+
+#[test]
+fn metric_name_and_description_as_expected() {
+    let metrics = metrics().static_metrics();
+    assert_eq!(metrics.len(), 2);
+    assert_eq!(metrics[0].name(), "cardinality");
+    assert_eq!(
+        metrics[0].description(),
+        Some("some description")
+    );
+}